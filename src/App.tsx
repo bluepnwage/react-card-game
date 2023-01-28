f2b5e5e39417f31bcd7ea8ece88fdd2effe21dd6
bluepnwage <a.carty2555@gmail.com>
2023-01-28 11:32:07 -0400

    add card animations and fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { createGame } from "./util/create-game";
-const game = createGame();
-
+import { motion } from "framer-motion";
 type Entry = {
   x: number;
   y: number;
@@ -17,7 +16,10 @@ type PlayPicks = {
 function App() {
   const [selected, setSelected] = useState<Partial<PlayPicks>>({});
   const [successfulSelections, setSuccessful] = useState<string[]>([]);
+  const [game, setGame] = useState(createGame());
   const [loading, setLoading] = useState(false);
+  const [moves, setMoves] = useState(0);
+  const [complete, setComplete] = useState(false);
   useEffect(() => {
     if (selected.first && selected.second) {
       const check = checkPosition();
@@ -35,19 +37,27 @@ function App() {
   }, [selected]);
 
   useEffect(() => {
-    if (successfulSelections.length === 6) alert("Game completed!!");
+    if (successfulSelections.length === 6) setComplete(true);
   }, [successfulSelections]);
 
   const onSelect = (value: Entry) => {
+    if (successfulSelections.includes(value.value)) return;
+    if (selected.first?.x === value.x && selected.first?.y === value.y) return;
+    if (selected.second?.x === value.x && selected.second.y === value.y) return;
     if (!selected.first) {
       setSelected((prev) => ({ ...prev, first: value }));
     } else {
       setSelected((prev) => ({ ...prev, second: value }));
+      setMoves((prev) => prev + 1);
     }
   };
 
   const checkPosition = () => {
-    return selected.first?.value === selected.second?.value;
+    return (
+      selected.first?.value === selected.second?.value &&
+      selected.first?.x !== selected.second?.x &&
+      selected.second?.y !== selected.first?.y
+    );
   };
 
   const checkSelected = (y: number, x: number) => {
@@ -56,8 +66,19 @@ function App() {
     return first || second;
   };
 
+  const resetGame = () => {
+    setSuccessful([]);
+    setComplete(false);
+    setGame(createGame());
+    setMoves(0);
+  };
+
   return (
     <div className="App">
+      <div>
+        <p>Moves: {moves}</p>
+        {complete && <button onClick={resetGame}>Reset</button>}
+      </div>
       <div className="game-wrapper">
         {game.map((position, yPosition) => {
           return (
@@ -66,14 +87,25 @@ function App() {
                 const checked = successfulSelections.includes(symbol);
                 const s = checkSelected(yPosition, xPosition);
                 return (
-                  <div
+                  <motion.div
+                    initial={false}
+                    animate={{ rotateY: s ? 0 : checked ? 0 : 180 }}
+                    transition={{ duration: 0.5 }}
                     onClick={!loading ? () => onSelect({ value: symbol, x: xPosition, y: yPosition }) : undefined}
                     className="game-card"
                     data-complete={checked}
                     key={xPosition}
                   >
-                    {s ? symbol : checked ? symbol : null}
-                  </div>
+                    {(s || checked) && (
+                      <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.25, duration: 0.25 }}
+                      >
+                        {symbol}
+                      </motion.span>
+                    )}
+                  </motion.div>
                 );
               })}
             </div>
